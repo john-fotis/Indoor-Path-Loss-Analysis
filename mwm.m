07c85561d7aed5f6bf8bf8dc13a6c7a54db27154
john-fotis <giannisfotis@gmail.com>
2021-08-17 00:32:23 +0300

    Minor Changes

--- mwm.m
+++ mwm.m
@@ -5,29 +5,31 @@
 %   - d: distance between Transmitter and Receiver [m]
 %   - numThin: number of thin walls between Transmitter and Receiver
 %   - numThick: number of thick walls between Transmitter and Receiver
-%   - nFloors: number of floors between Transmitter and Receiver
+%   - numFloors: number of floors between Transmitter and Receiver
 %
 %   Return value:
-%   - mwmLoss: signal strength loss [dB]
+%   - PL: total path loss [dB]
 %
 % *************************************************************************
 
 function PL = mwm(f, d, numThin, numThick, numFloors)
 
-    Lc = 0;             % (dB)
-    thinLoss = 3.4;     % (dB)
-    thickLoss = 6.9;    % (dB)
+    Lc = 0;                 % (dB)
+    thinWallLoss = 3.4;     % (dB)
+    thickWallLoss = 6.9;    % (dB)
     b = 0.46;
-    Lf = 18.3;
+    Lf = 18.3;              % (dB)
 
     % Speed of light (divided by 10^6 because f is in MHz)
     c = 299.79246;
-    
-    % Calculate free space loss (dB)
-    LFS = 20*log10(d) + 20*log10(f) + 20*log10((4*pi)/c);
 
-    exp =(numFloors+2)/(numFloors+1) - b;
+    if (f >= 800) && (f <= 2000)
+        % Calculate free space loss (dB)
+        LFS = 20*log10(d) + 20*log10(f) + 20*log10((4*pi)/c);
 
-    PL = LFS + Lc + numThin*thinLoss + numThick*thickLoss + numFloors^exp*Lf;
-    
-end
+        exp =(numFloors+2)/(numFloors+1) - b;
+
+        PL = LFS + Lc + numThin*thinWallLoss + numThick*thickWallLoss + numFloors^exp*Lf;
+    else
+        PL = 10e14;
+    end
